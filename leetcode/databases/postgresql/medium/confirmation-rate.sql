7f7202a4787af48add460404854815fed733c901
Roman Semeniuk <roman.semeniuk@novage.com.ua>
2024-09-18 16:06:35 +0300

    Add confirmation rate (1934) solution

---
+++ leetcode/databases/postgresql/medium/confirmation-rate.sql
@@ -0,0 +1,15 @@
+-- problem: 1934. Confirmation Rate
+
+-- Runtime 335 ms Beats 24.47%
+-- Memory 0.00 MB Beats 100.00%
+
+SELECT s.user_id,
+       CASE
+           WHEN COUNT(c.user_id) = 0 THEN 0.00::numeric
+           WHEN SUM(CASE WHEN c.action = 'confirmed' THEN 1::numeric ELSE 0::numeric END) = COUNT(c.user_id) THEN ROUND(1, 2)
+           ELSE ROUND(SUM(CASE WHEN c.action = 'confirmed' THEN 1::numeric ELSE 0::numeric END) / COUNT(c.user_id), 2)
+       END AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
+
