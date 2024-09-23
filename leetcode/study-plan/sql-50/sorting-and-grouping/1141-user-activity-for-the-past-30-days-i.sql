25dcc421328ceaeb31cd71736e3da4a41e0d7906
Roman Semeniuk <roman.semeniuk@novage.com.ua>
2024-09-23 12:33:36 +0300

    Add improved User Activity for the Past 30 Days I solution

--- leetcode/study-plan/sql-50/sorting-and-grouping/1141-user-activity-for-the-past-30-days-i.sql
+++ leetcode/study-plan/sql-50/sorting-and-grouping/1141-user-activity-for-the-past-30-days-i.sql
@@ -16,3 +16,20 @@ GROUP BY
     1
 ORDER BY
     1;
+
+-- PostgreSQL
+
+-- problem: 1141. User Activity for the Past 30 Days I
+
+-- Runtime 368 ms Beats 83.91%
+-- Memory 0.00 MB Beats 100.00%
+
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM
+    Activity
+WHERE
+    activity_date BETWEEN '2019-07-27'::DATE - 29 AND '2019-07-27'::DATE
+GROUP BY
+    activity_date;
