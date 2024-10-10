6a63733ff48d1f40ed44db58065ae5037528717c
Roman Semeniuk <roman.semeniuk@novage.com.ua>
2024-10-10 20:25:19 +0300

    Add optimized 1164. Product Price at a Given Date solution

--- leetcode/study-plan/sql-50/advanced-select-and-joins/1164-product-price-at-a-given-date.sql
+++ leetcode/study-plan/sql-50/advanced-select-and-joins/1164-product-price-at-a-given-date.sql
@@ -1,6 +1,6 @@
 -- PostgreSQL
 
--- problem: 1164. Product Price at a Given Date
+-- Problem: 1164. Product Price at a Given Date
 
 -- Runtime 383 ms Beats 69.74%
 -- Memory 0.00 MB Beats 100.00%
@@ -24,3 +24,26 @@ WHERE (product_id, change_date) IN (
         WHERE change_date <= '2019-08-16'
         GROUP BY 1
 );
+
+-- PostgreSQL
+
+-- Problem: 1164. Product Price at a Given Date
+
+-- Runtime 365 ms Beats 81.08%
+-- Memory 0.00 MB Beats 100.00%
+
+SELECT
+    p1.product_id,
+    COALESCE(
+        (
+            SELECT new_price
+            FROM Products p2
+            WHERE p1.product_id = p2.product_id
+            AND p2.change_date <= '2019-08-16'
+            ORDER BY p2.change_date DESC
+            LIMIT 1
+        ),
+        10
+    ) AS price
+FROM
+    (SELECT DISTINCT product_id FROM Products) p1;
