a95cbd76ea7cc6b1a13add2fd8474d8e300eb6bf
Roman Semeniuk <roman.semeniuk@novage.com.ua>
2024-10-16 21:39:22 +0300

    Add 28. Find the Index of the First Occurrence in a String solution

---
+++ leetcode/study-plan/programming-skills/basic-implementation/28-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,6 @@
+// Problem: 28. Find the Index of the First Occurrence in a String
+
+// Runtime 49ms Beats 91.01%
+// Memory 49.91 MB Beats 94.29%
+
+const strStr = (t: string, s: string): number => t.indexOf(s);
