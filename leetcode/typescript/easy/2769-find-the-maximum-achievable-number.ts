3a27fdc48d6b95075abc752c2d7588604783ab11
Roman Semeniuk <roman.semeniuk@novage.com.ua>
2024-11-04 22:51:02 +0200

    Add 2769. Find the Maximum Achievable Number solution.

---
+++ leetcode/typescript/easy/2769-find-the-maximum-achievable-number.ts
@@ -0,0 +1,6 @@
+// Problem: 2769. Find the Maximum Achievable Number
+
+// Runtime 1ms Beats 100%
+// Memory 54.42 MB Beats 63.41%
+
+const theMaximumAchievableX = (num: number, t: number): number => num + t * 2;
